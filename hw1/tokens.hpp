f53a2a9f130880f6eac8e648e8c67595d9f093e2
Ariel Elya <arielelya2@gmail.com>
2022-05-02 10:48:30 +0300

    all the files of hw1 moved to one directory

---
+++ hw1/tokens.hpp
@@ -0,0 +1,53 @@
+
+#ifndef TOKENS_HPP_
+#define TOKENS_HPP_
+
+#include <string>
+#include <cstdlib>
+  enum tokentype
+  {
+    VOID = 1,
+    INT = 2,
+    BYTE = 3,
+    B = 4,
+    BOOL = 5,
+    AND = 6,
+    OR = 7,
+    NOT = 8,
+    TRUE = 9,
+    FALSE = 10,
+    RETURN = 11,
+    IF = 12,
+    ELSE = 13,
+    WHILE = 14,
+    BREAK = 15,
+    CONTINUE = 16, 
+    SC = 17,
+    COMMA = 18,
+    LPAREN = 19,
+    RPAREN = 20,
+    LBRACE = 21,
+    RBRACE = 22,
+    ASSIGN = 23,
+    RELOP = 24,
+    BINOP = 25,
+    COMMENT = 26,
+    ID = 27,
+    NUM = 28,
+    STRING = 29,
+	  AUTO=30,
+    ERORR_UNCLOSED_STRING = 31,
+    ERORR_UNDEFINED_ESCAPE_SEQ_STRING = 32,
+    ERORR_UNCLOSED_UNDEFINED_ESCAPE_SEQ_CHAR = 33,
+    ERORR_UNCLOSED_UNDEFINED_ESCAPE_SEQ_NOT_HEX_DECODE =34,
+    ERORR_UNCLOSED_UNDEFINED_ESCAPE_SEQ_NOT_ROGHT_LEN_HEX = 35,
+    ERORR_UNCLOSED_UNDEFINED_ESCAPE_SEQ_EOF = 36
+  };
+  
+  extern int yylineno;
+  extern char* yytext;
+  extern int  yyleng;
+  extern int yylex();
+  extern std::string newString;
+
+#endif /* TOKENS_HPP_ */

885a94928f4f24cf1071e82ebefad538daa88b10
Assaf Lovton <62953134+assaflovton@users.noreply.github.com>
2022-06-18 18:44:34 +0300

    scanner lex updated

--- hw5/scanner.h
+++ hw5/scanner.h
@@ -13,29 +13,57 @@ void errorLexAndExit(int lineno)
     exit(0);
 }
 
-int LexToken(int bison_enum, string token) {
+int LexToken(int bison_enum, string token)
+{
     BaseTypePtr result = nullptr;
 
-    switch (bison_enum) {
-        case NUM:
-            result.reset(new NumberType(token));
-            break;
-        case MULT_DIV:
-        case PLUS_MINUS:
-        case ID:
-        case STRING:
-        case INEQUALITY:
-        case EQUAL_UNEQUAL:
-            result.reset(new StringType(token));
-            break;
-        default:
-            break;
+    switch (bison_enum)
+    {
+    case NUM:
+    {
+        result.reset(new NumberType(token));
+    }
+    break;
+    // case AUTO:
+    // {
+    //     result.reset(new AutoType(token));
+    // }
+    // break;
+    case STRING:
+    {
+        result.reset(new StringType(token));
+    }
+    break;
+    case INEQUALITY:
+    {
+        result.reset(new StringType(token));
+    }
+    break;
+    case MULT_DIV:
+        result.reset(new StringType(token));
+        break;
+    case PLUS_MINUS:
+        result.reset(new StringType(token));
+        break;
+
+    case ID:
+    {
+        result.reset(new StringType(token));
+    }
+    break;
+    case EQUAL_UNEQUAL:
+    {
+        result.reset(new StringType(token));
+    }
+    break;
+
+    default:
+        break;
     }
 
     yylval = result;
 
     return bison_enum;
 }
-
 
 #endif // HW3_SCANNER_H
